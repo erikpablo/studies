ed7913429a77a76cfae718a74b7e6ee747e8c165
Erik Pablo <nunespablo470@gmail.com>
2025-03-31 08:30:05 -0300

    Propriedades Opcionais

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,11 @@
 //Types em Objetos
 
-function resumo(usuario: {name: string, age: number}) {
-    return `Olá ${usuario.name}, tudo bem? Você tem ${usuario.age} anos`;
+function resumo(usuario: {name: string, age?: number}) {
+    if(usuario.age == undefined) {
+        return `Olá ${usuario.name}, tudo bem? Você tem ${usuario.age} anos`;
+    } else {
+        return `Olá ${usuario.name}, tudo bem?`
+    }
 }
 
 let u = {
