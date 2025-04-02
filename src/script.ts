b97b8c95ef69d7ea9771d68af89246bec2d9e899
Erik Pablo <nunespablo470@gmail.com>
2025-04-02 08:56:00 -0300

    Type function

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,17 @@
-//Type Assertions
+//Type para funções
 
-let ageField = document.querySelector('idade') as HTMLInputElement;
+type MathFunction = (n1: number, n2: number) => number;
 
-console.log(ageField.value)
+const somar: MathFunction = (n1, n2) => {
+    return console.log(n1 + n2);
+}
+const subt: MathFunction = (n1, n2) => {
+    return console.log(n1 - n2);
+}
+const mult: MathFunction = (n1, n2) => {
+    return console.log(n1 * n2);
+}
+const div: MathFunction = (n1, n2) => {
+    return console.log(n1 / n2);
+}
+
